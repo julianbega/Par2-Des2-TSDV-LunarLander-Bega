62795a4a166ae530b15c82f6f630d2a1376396be
Julian Bega <julian.begamilrad@gmail.com>
2021-06-29 01:22:52 -0300

    Player stats

---
+++ LunarLander-P2-BegaJulian/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public float fuel;
+    public float score;
+    public float rotationSpeed;
+    public float propultionSpeed;
+
+    private float horizontalSpeed;
+    private float verticalSpeed;
+    private float Altitud;
+    private bool isPaused;
+
+    public Rigidbody2D playerRB;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        verticalSpeed = playerRB.velocity.y;
+        horizontalSpeed = playerRB.velocity.x;
+    }
+}
