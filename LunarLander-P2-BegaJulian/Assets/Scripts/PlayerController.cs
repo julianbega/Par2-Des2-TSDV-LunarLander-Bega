6a32ba9bec8260f1694a165883d466ba1a013852
Julian Bega <julian.begamilrad@gmail.com>
2021-06-29 01:23:03 -0300

    PlayerControler

---
+++ LunarLander-P2-BegaJulian/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public PlayerStats stats;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        { stats.playerRB.transform.Rotate(Vector3.forward * Time.deltaTime * stats.rotationSpeed, Space.Self); }
+        if (Input.GetKey(KeyCode.D))
+        { stats.playerRB.transform.Rotate(-Vector3.forward * Time.deltaTime * stats.rotationSpeed, Space.Self); }
+
+        if (Input.GetKey(KeyCode.Space) && stats.fuel > 0)
+        { 
+            stats.playerRB.AddForce(transform.up * Time.deltaTime * stats.propultionSpeed);
+            stats.fuel -= 10 * Time.deltaTime;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       // if(stats.playerRB.velocity.x >)
+    }
+}
