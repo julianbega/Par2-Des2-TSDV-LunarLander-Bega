c98e9d353fb6e70a90b2c863e25d086a4058b3bf
Julian Bega <julian.begamilrad@gmail.com>
2021-06-29 01:23:31 -0300

    Game manager

--- LunarLander-P2-BegaJulian/Assets/Scripts/GameManager.cs
+++ LunarLander-P2-BegaJulian/Assets/Scripts/GameManager.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject Map;
+    public GameObject Player;
+    public float time;
+    public bool isPaused;
     // Start is called before the first frame update
     void Start()
     {
-        
+        UpdateMap();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void UpdateMap()
+    {
+        float randomXpos = Random.Range(0, 110.5f);
+        Map.transform.position = new Vector3(randomXpos * -1, 1.3f, 0);
     }
 }
