beeb9cdfc0aa08ec7516b4858d05070fa261b208
Julian Bega <julian.begamilrad@gmail.com>
2021-06-29 18:06:11 -0300

    SceneManager

---
+++ LunarLander-P2-BegaJulian/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneManager : MonoBehaviour
+{
+    public static SceneManager instanceSceneManager;
+
+    public static SceneManager Instance { get { return instanceSceneManager; } }
+
+    private void Awake()
+    {
+        if (instanceSceneManager != null && instanceSceneManager != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instanceSceneManager = this;
+        }
+    }
+
+    private void Start()
+    {
+    }
+    private void OnDisable()
+    {
+    }
+
+    public void ChangeScene(string scene)
+    {
+        Debug.Log("Cambia de escena a" + scene);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(scene);
+    }
+
+    public void OnClickQuit()
+    {
+        Application.Quit();
+    }
+}
