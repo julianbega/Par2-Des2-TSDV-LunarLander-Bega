daeb31a7543624be314e9cda6ba7244a174d4d75
Julian Bega <julian.begamilrad@gmail.com>
2021-06-29 18:03:13 -0300

    Player Update

--- LunarLander-P2-BegaJulian/Assets/Scripts/PlayerController.cs
+++ LunarLander-P2-BegaJulian/Assets/Scripts/PlayerController.cs
@@ -30,4 +30,6 @@ public class PlayerController : MonoBehaviour
     {
        // if(stats.playerRB.velocity.x >)
     }
+
+
 }
---
+++ LunarLander-P2-BegaJulian/Assets/Scripts/ZoomManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomManager : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("entra");
+        if (collision.tag == "Map")
+        {
+            CameraZoomIn();
+            Debug.Log("entra");
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Map")
+        {
+            CameraZoomOut();
+
+            Debug.Log("entra");
+        }
+    }
+
+    private void CameraZoomOut()
+    {
+
+    }
+    private void CameraZoomIn()
+    {
+
+    }
+}
