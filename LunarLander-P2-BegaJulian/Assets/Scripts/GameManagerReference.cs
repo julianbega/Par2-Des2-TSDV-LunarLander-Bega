bef4c32eff914e1b2956b6c142e330b93f16fe00
Julian Bega <julian.begamilrad@gmail.com>
2021-06-30 04:25:25 -0300

    Pause

---
+++ LunarLander-P2-BegaJulian/Assets/Scripts/GameManagerReference.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerReference : MonoBehaviour
+{
+    private GameManager referenceManager;
+    private void Awake()
+    {
+        referenceManager = null;
+    }
+    void Start()
+    {
+        referenceManager = FindObjectOfType<GameManager>();
+    }
+    void LateStart()
+    {
+        if (referenceManager == null)
+            referenceManager = FindObjectOfType<GameManager>();
+    }
+
+    public void PauseGame()
+    {
+        referenceManager.Pause();
+    }
+}
