78ce9719de8839efbb4560e9dc9219ec561a42ac
Julian Bega <julian.begamilrad@gmail.com>
2021-07-13 19:35:31 -0300

    Wind zones

--- LunarLander-P2-BegaJulian/Assets/Scripts/Managers/ZoomManager.cs
+++ LunarLander-P2-BegaJulian/Assets/Scripts/Managers/ZoomManager.cs
@@ -11,12 +11,19 @@ public class ZoomManager : MonoBehaviour
     public float zoomSpeed;
     public bool isZoomed;
     public bool collide;
+    public Vector3 viewportPos;
+    public float xCameraHighLimit;
+    public float xCameraLowLimit;
     private void Start()
     {
         mainCamera = Camera.main;
         initialCameraPos = mainCamera.transform.position;
         isZoomed = false;
         collide = false;
+        viewportPos = mainCamera.transform.position;
+
+        xCameraLowLimit = viewportPos.x - ((Screen.width / 10) * 3);
+        xCameraHighLimit = viewportPos.x + ((Screen.width / 10) * 3);
     }
     void FixedUpdate()
     { 
@@ -36,8 +43,6 @@ public class ZoomManager : MonoBehaviour
                 isZoomed = false;
             }
         }
-        Vector3 viewportPos = mainCamera.WorldToViewportPoint(this.transform.position);
-       // Debug.Log("pos en viewport = " + viewportPos.x + " / " + viewportPos.y);
     }
     private void CameraZoomOut()
     {
@@ -45,8 +50,20 @@ public class ZoomManager : MonoBehaviour
         mainCamera.orthographicSize = Mathf.Lerp(mainCamera.orthographicSize, 5, 1);
     }
     private void CameraZoomIn()
-    {     
-       mainCamera.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, mainCamera.transform.position.z);
-       mainCamera.orthographicSize = Mathf.Lerp(mainCamera.orthographicSize, 2, zoomSpeed);
+    {   
+        if (this.transform.position.x < xCameraLowLimit)
+        {
+            mainCamera.transform.position = new Vector3(xCameraLowLimit, this.transform.position.y, mainCamera.transform.position.z);
+        }
+        else if (this.transform.position.x > xCameraHighLimit)
+        {
+            mainCamera.transform.position = new Vector3(xCameraHighLimit, this.transform.position.y, mainCamera.transform.position.z);
+        }
+        else 
+        {
+            mainCamera.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, mainCamera.transform.position.z);
+        }
+        mainCamera.orthographicSize = Mathf.Lerp(mainCamera.orthographicSize, 2, zoomSpeed);
+       
     }
 }
