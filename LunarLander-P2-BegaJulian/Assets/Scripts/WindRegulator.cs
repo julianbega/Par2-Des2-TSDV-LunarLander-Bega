f7d6764a15392daf12a43993462993fca54e6318
Julian Bega <julian.begamilrad@gmail.com>
2021-07-13 20:09:10 -0300

    Wind editable
    se puede cambiar el funcionamiento del viento desde el script windRegulator en el prefab

---
+++ LunarLander-P2-BegaJulian/Assets/Scripts/WindRegulator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindRegulator : MonoBehaviour
+{
+    PlayerManager lvlGiver;
+    public AreaEffector2D effect;
+    int lvl;
+    public int lvlToStartLowWind;
+    public int lvlToStandardWind;
+    public int lvlToStartHighWind;
+    public float lowWind;
+    public float standardWind;
+    public float highWind;
+    void Start()
+    {
+        lvlGiver = FindObjectOfType<PlayerManager>();
+    }
+    void Update()
+    {
+        if (lvlGiver == null)
+        {
+            lvlGiver = FindObjectOfType<PlayerManager>();
+        }
+        lvl = lvlGiver.lvl;
+
+        if (lvl < lvlToStartLowWind)
+        {
+            effect.forceMagnitude = 0;
+        }
+        if (lvl > lvlToStartLowWind && lvl <= lvlToStandardWind)
+        {
+            effect.forceMagnitude = lowWind;
+        }
+        if (lvl > lvlToStandardWind && lvl <= lvlToStartHighWind)
+        {
+            effect.forceMagnitude = standardWind;
+        }
+        if (lvl >= lvlToStartHighWind)
+        {
+
+            effect.forceMagnitude = highWind;
+        }
+    }
+}
